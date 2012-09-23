8fdae67ac173cd4a75c6e2eb5629336dfae04b1c
Enrico Ballardini <lamemind@gmail.com>
2012-09-23 16:40:00 +0300

    Update make-import.php

--- make-import.php
+++ make-import.php
@@ -14,4 +14,4 @@ $import = new ImportGenerator ();
 if (!$import->loadConfigFile () || !$import->build ())
 	die ($import->getError () . "\n\n");
 
-echo "import-file `" . $import->getDestFilepath () . "` built successfully\n\n";
+echo ">>> import-file `" . $import->getDestFilepath () . "` built successfully\n\n";
